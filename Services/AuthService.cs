c98cde7e6a40f69afd79b72eb06cf4bbdc6be18a
aleensd <aleen-sd@outlook.com>
2023-06-24 12:47:47 +0300

    Generate Token

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UserSubscriptionWebApi.Configurations;
 using UserSubscriptionWebApi.IServices;
 using UserSubscriptionWebApi.Models;
 using UserSubscriptionWebApi.Models.DTOs;
@@ -10,9 +15,13 @@ namespace UserSubscriptionWebApi.Services
     {
         private readonly UserManager<IdentityUser> _userManager;
 
-        public AuthService(UserManager<IdentityUser> userManager)
+        private readonly IConfiguration _configuration;
+
+
+        public AuthService(UserManager<IdentityUser> userManager, IConfiguration configuration)
         {
             _userManager = userManager;
+            _configuration = configuration;
         }
 
         public async Task<AuthResult> Register(UserRegistrationRequestDTO requestDTO)
@@ -42,13 +51,16 @@ namespace UserSubscriptionWebApi.Services
             var is_created = await _userManager.CreateAsync(new_user, requestDTO.Password);
             if (is_created.Succeeded)
             {
-                var user_role = await _userManager.AddToRoleAsync(new_user, "user");
+               await _userManager.AddToRoleAsync(new_user, "user");
+
+                //Generate token
+                var token = GenerateToken(new_user, "USER");
+
                 return new AuthResult()
                 {
+                    Token = token,
                     Result = true,
                 };
-                //Generate token
-
             }
 
             return new AuthResult()
@@ -60,6 +72,33 @@ namespace UserSubscriptionWebApi.Services
                         }
 
             };
+        }
+
+        private string GenerateToken(IdentityUser user, string role)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_configuration.GetSection("JwtConfig:Secret").Value);
+
+            //Token descriptor
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("Id", user.Id),
+                    new Claim(JwtRegisteredClaimNames.Sub,user.Email),
+                    new Claim(JwtRegisteredClaimNames.Email,user.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Iat,DateTime.Now.ToUniversalTime().ToString()),
+                    new Claim(ClaimTypes.Role,role),
+
+                }),
+                Expires = DateTime.Now.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+            };
+
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+            var jwtToken = jwtTokenHandler.WriteToken(token);
+            return jwtToken;
         }
     }
 
